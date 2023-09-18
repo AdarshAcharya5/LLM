212307b957a11ef1bb7cbc0936c8f9bf45d8cda5
Adarsh Acharya <adarsh.acharya1x@gmail.com>
2023-09-18 16:35:14 +0530

    Added MultiHeadAttention Unit

---
+++ MultiHeadAttention.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from SelfAttention import SelfAttention
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads, head_size, embed_size, dropout_=0.2):
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_size = head_size
+        self.embed_size = embed_size
+        self.heads = nn.ModuleList([SelfAttention(self.head_size) for _ in range(self.num_heads)])
+        self.linear = nn.Linear(self.num_heads * self.head_size, self.embed_size)
+        self.dropout = nn.Dropout(dropout_)
+
+    def forward(self, x):
+        x = torch.cat([head(x) for head in self.heads], dim=-1)
+        x = self.dropout(self.linear(x))
+        return x
